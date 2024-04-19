003971a0725217abddf6088a2181d56000ecfa75
Aditi Hedau <Aditi@cdac.in>
2024-04-20 00:05:31 +0530

    First file created

---
+++ Addition.java
@@ -0,0 +1,12 @@
+public class Addition
+{
+	public static void main(String args[])
+	{
+		int number1 = 10;  
+		int number2 = 20;
+		int add;
+		add = number1 + number2;
+		System.out.println("Addition of Numbers");
+		System.out.println(add);
+	}
+}
